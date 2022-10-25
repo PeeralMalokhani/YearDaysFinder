96bb23286beb4f0d0d8d7e530bb52cac2879ddf0
PeeralMalokhani <91820725+PeeralMalokhani@users.noreply.github.com>
2022-10-25 10:28:42 +0500

    Add files via upload

---
+++ AgeCalculator.js
@@ -0,0 +1,8 @@
+var current_age=prompt("enter your current_age in interger form");
+var days=current_age*365;
+var week=days/8;
+ week=Math.floor(week)
+var month=current_age*12;
+console.log("you are become  "+month+" months.");
+console.log(days+" days ");
+console.log(week+" weeks ");
